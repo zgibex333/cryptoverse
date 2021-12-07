ff4c9e1fc4b7f93b6692112f2e5fb3c67945bec2
Den <zgibex333@gmail.com>
2021-12-07 22:07:58 +0300

    footer layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 import "./App.css";
 
-
 // components
 import {
   Navbar,
@@ -26,10 +25,13 @@ const App = () => {
             <Routes>
               <Route exact path="/" element={<Homepage />} />
               <Route exact path="/exchanges" element={<Exchanges />} />
-              <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
               <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
               <Route exact path="/news" element={<News />} />
-           
 
               {/* <Route exact path="/exchanges">
                 <Exchanges />
@@ -46,8 +48,21 @@ const App = () => {
             </Routes>
           </div>
         </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   );
 };
